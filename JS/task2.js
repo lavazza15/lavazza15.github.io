5f4fc6874d9ebedf73393bd269972dea738a874c
lavazza15 <ulyana.yuzko@gmail.com>
2020-07-18 21:07:06 +0300

    update

--- JS/task2.js
+++ JS/task2.js
@@ -15,11 +15,42 @@ function generateFibonacciSequence (n) {
 }
 
 //функія для перемішування елементів масиву
-var names = [];
+
 function ShuffleArray(names) {
     for (var i = names.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
       [names[i], names[j]] = [names[j], names[i]];
     }
       return names;
+}
+
+//задача з презентації
+
+var array = [-4, 48, 22, -22, 15, 7, 155, -3, 18, 99];
+
+function maxElement(array) {
+    var sumP = 0;
+    var prodN = 1;
+    var countP = 0;
+    var countN = 0;
+
+    for(var i = 0; i < array.length; i++) {
+        if (array[i]>=0) {
+            countP++;
+            sumP += array[i];
+        }
+        else {
+            countN++;
+            prodN *= array[i];
+        }
+    }
+
+    return {
+        max: Math.max(...array),
+        min: Math.min(...array),
+        sumP: sumP,
+        prodN: prodN,
+        countP: countP,
+        countN: countN
+    };
 }
