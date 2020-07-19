736380ccc8d043e6b8a2b11794189163a9fcac8c
lavazza15 <ulyana.yuzko@gmail.com>
2020-07-19 15:05:54 +0300

    chess_JS

---
+++ JS/chess_JS/chess.js
@@ -0,0 +1,17 @@
+for (var i = 1; i <= 8; i++) {
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
+
+    for (var j = 1; j<=8; j++) {
+        var cell = document.createElement("div");
+        var $class = "";
+        $class = (j % 2 !== 0 & i % 2 !==0)? "cell cell-white": "cell";
+        $class = (j % 2 == 0 & i % 2 !==0)? "cell cell-black": $class;
+        $class = (j % 2 !== 0 & i % 2 ==0)? "cell cell-black": $class;
+        $class = (j % 2 == 0 & i % 2 !==0)? "cell cell-white": $class;
+        cell.setAttribute("class", $class);
+       
+        row.appendChild(cell);
+    }
+    document.getElementById("container").appendChild(row);
+}
