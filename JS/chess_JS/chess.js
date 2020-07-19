6ba193c2c3bfda0877fe03694ec2d306e999ee37
lavazza15 <ulyana.yuzko@gmail.com>
2020-07-19 16:11:28 +0300

    update

--- JS/chess_JS/chess.js
+++ JS/chess_JS/chess.js
@@ -5,10 +5,10 @@ for (var i = 1; i <= 8; i++) {
     for (var j = 1; j<=8; j++) {
         var cell = document.createElement("div");
         var $class = "";
-        $class = (j % 2 !== 0 & i % 2 !==0)? "cell cell-white": "cell";
-        $class = (j % 2 == 0 & i % 2 !==0)? "cell cell-black": $class;
-        $class = (j % 2 !== 0 & i % 2 ==0)? "cell cell-black": $class;
-        $class = (j % 2 == 0 & i % 2 !==0)? "cell cell-white": $class;
+        $class = ((j % 2 !== 0) && (i % 2 !==0))? "cell cell-white": "cell";
+        $class = ((j % 2 == 0) && (i % 2 !==0))? "cell cell-black": $class;
+        $class = ((j % 2 !== 0) && (i % 2 ==0))? "cell cell-black": $class;
+        $class = ((j % 2 == 0) && (i % 2 !==0))? "cell cell-white": $class;
         cell.setAttribute("class", $class);
        
         row.appendChild(cell);
