a6c6a1dfbec89f600ef504fe000c4bf27c572a40
lavazza15 <ulyana.yuzko@gmail.com>
2020-07-31 18:58:09 +0300

    currency value

---
+++ JS/CurrValue/value.js
@@ -0,0 +1,44 @@
+var btnGet = document.getElementById("btnGet");
+var result = document.getElementById("result");
+btnGet.addEventListener("click", getCurrencyValue);
+
+function dateToStringNBU(dateObj) {
+    function add0(value) {
+        return (value.toString().length < 2) ? `0${value}` : value.toString();
+    }
+    if (dateObj instanceof Date) {
+        return `${dateObj.getFullYear()}${add0(dateObj.getMonth() + 1)}${add0(dateObj.getDate())}`;
+    }
+    return false;
+}
+
+
+function getCurrencyValue() {
+    const ONEDAY = 24 * 60 * 60 * 1000; // amount of miliseconds in one day
+    var sDate = new Date(document.getElementById("sdate").value);
+    var eDate = new Date(document.getElementById("edate").value);
+    var currency = document.getElementById("currency").value;
+
+    var diff = (eDate - sDate) / ONEDAY;
+    var cDate;
+    let i = 0;
+    while(i <= diff) {
+        cDate = new Date(sDate.valueOf() + (i * ONEDAY));
+        i++;
+        var URI = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${currency}&date=${dateToStringNBU(cDate)}&json`;
+        var XHR = new XMLHttpRequest();
+        XHR.open("GET", URI);
+        XHR.send();
+        XHR.addEventListener("readystatechange", function () {
+        if ((XHR.readyState === 4) && (XHR.status === 200)) {
+            var data = JSON.parse(XHR.responseText);
+            result.innerHTML = `<h1>${data[0].rate}</h1>`;
+        }
+    })
+    }
+
+    
+
+    
+}
+
